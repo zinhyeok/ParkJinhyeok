fa3fb93d1117529985b98e072747682b9764f79e
zinhyeok <david9pjh@gmail.com>
2021-07-08 20:42:48 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,13 @@
 # write by zinhyeok david9pjh@gmail.com
-num  = 0
-call_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+num = 0
+while(num == 0):
+    try:
+        num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        if num > 3 or num < 0:
+            raise IndexError
+    except ValueError:
+        print('정수를 입력하세요')
+        num = 0
+    except IndexError:
+        print('1, 2, 3 중 하나를 입력하세요')
+        num = 0
