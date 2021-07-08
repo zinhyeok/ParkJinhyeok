d3330998e6e7299d54ec5b325ee221bb200896fe
zinhyeok <david9pjh@gmail.com>
2021-07-08 21:59:24 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,16 +1,33 @@
 # write by zinhyeok david9pjh@gmail.com
-num = 0
-while(num == 0):
-    try:
-        num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-        if num > 3 or num < 0:
-            raise IndexError
-    except ValueError:
-        print('정수를 입력하세요')
-        num = 0
-    except IndexError:
-        print('1, 2, 3 중 하나를 입력하세요')
-        num = 0
 
-for i in range(num):
-    print("playerA: {}".format(i+1))
+round_num = 1
+current_num = 0
+# while문 임의로 반복횟수 할당
+while(round_num < 10):
+    player_num = 0
+    # change playerName by round(odd, even)
+    if round_num % 2 == 1:
+        playerName = "A"
+    else:
+        playerName = "B"
+
+    # input baskin num
+    while(player_num == 0):
+        try:
+            player_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+            if player_num > 3 or player_num < 0:
+                raise IndexError
+
+        except ValueError:
+            print('정수를 입력하세요')
+            player_num = 0
+        except IndexError:
+            print('1, 2, 3 중 하나를 입력하세요')
+            player_num = 0
+
+    # print num, add currnt_num
+    for i in range(player_num):
+        current_num = current_num + 1
+        print("player{}: {}".format(playerName, current_num))
+
+    round_num = round_num + 1
