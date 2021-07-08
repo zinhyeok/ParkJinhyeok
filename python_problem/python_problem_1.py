1526e42127891fc3a0a6d05f851e8e8781934c40
zinhyeok <david9pjh@gmail.com>
2021-07-09 00:46:11 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,7 @@
 # write by zinhyeok david9pjh@gmail.com
+import random
+
+
 def brGame():
     round_num = 1
     current_num = 0
@@ -7,34 +10,44 @@ def brGame():
         player_num = 0
         # change playerName by round(odd, even)
         if round_num % 2 == 1:
-            playerName = "A"
+            playerName = "player"
+            while(player_num == 0):
+                try:
+                    player_num = int(
+                        input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+                    if player_num > 3 or player_num < 0:
+                        raise IndexError
+
+                except ValueError:
+                    print('정수를 입력하세요')
+                    player_num = 0
+                except IndexError:
+                    print('1, 2, 3 중 하나를 입력하세요')
+                    player_num = 0
+
+            # print num, add currnt_num
+            for i in range(player_num):
+                current_num = current_num + 1
+                print("{}: {}".format(playerName, current_num))
+
+            round_num = round_num + 1
+
+        # computer play
         else:
-            playerName = "B"
-        # input baskin num
-        while(player_num == 0):
-            try:
-                player_num = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-                if player_num > 3 or player_num < 0:
-                    raise IndexError
+            playerName = "computer"
+            player_num = random.randint(1, 3)
 
-            except ValueError:
-                print('정수를 입력하세요')
-                player_num = 0
-            except IndexError:
-                print('1, 2, 3 중 하나를 입력하세요')
-                player_num = 0
+            # print num, add currnt_num
+            for i in range(player_num):
+                current_num = current_num + 1
+                print("{}: {}".format(playerName, current_num))
 
-        # print num, add currnt_num
-        for i in range(player_num):
-            current_num = current_num + 1
-            print("player{}: {}".format(playerName, current_num))
-
-        round_num = round_num + 1
+            round_num = round_num + 1
 
     if round_num % 2 == 1:
-        print("playerA win!")
+        print("player win!")
     else:
-        print("playerB win!")
+        print("computer win!")
 
 
 brGame()
