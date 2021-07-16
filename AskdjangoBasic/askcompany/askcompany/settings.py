b5cbe3379d5cf5e3a11a82e8634e0e886f0fec9b
zinhyeok <david9pjh@gmail.com>
2021-07-16 21:55:59 +0900

    5-8강

--- AskdjangoBasic/askcompany/askcompany/settings.py
+++ AskdjangoBasic/askcompany/askcompany/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os, json
+import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
--- AskdjangoBasic/askcompany/askcompany/urls.py
+++ AskdjangoBasic/askcompany/askcompany/urls.py
@@ -2,11 +2,7 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 
-def mysum(request, x, y):
-    result = x+y
-    return HttpResponse('result = {}'.format(result))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mysum/<int:x>/<int:y>/', mysum),
 ]
