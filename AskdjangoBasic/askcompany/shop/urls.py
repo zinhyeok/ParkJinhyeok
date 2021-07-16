fed16778f05221f028490da25b3f98414e1e5b4b
zinhyeok <david9pjh@gmail.com>
2021-07-17 08:40:57 +0900

    13강_수정

--- AskdjangoBasic/askcompany/shop/urls.py
+++ AskdjangoBasic/askcompany/shop/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-
+from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    # path('item/',item_list,name="item_list") -> item/주소, item_list 함수 호출, url(?) 이름은 "item_list"
-    path('achieve/<yyyy:year>/', views.achieve_year),
-    path('', views.item_list),
+
 ]
