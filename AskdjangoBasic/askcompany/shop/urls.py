bd05ee3b5a8562b5a92d1890e4eecea249f7a33b
zinhyeok <david9pjh@gmail.com>
2021-07-17 07:50:36 +0900

    13강

--- AskdjangoBasic/askcompany/shop/urls.py
+++ AskdjangoBasic/askcompany/shop/urls.py
@@ -4,5 +4,7 @@ from django.urls.resolvers import URLPattern
 app_name = 'shop'
 
 urlpatterns = [
-
+    # path('item/',item_list,name="item_list") -> item/주소, item_list 함수 호출, url(?) 이름은 "item_list"
+    path('achieve/<yyyy:year>/', views.achieve_year),
+    path('', views.item_list),
 ]
--- AskdjangoBasic/askcompany/shop/views.py
+++ AskdjangoBasic/askcompany/shop/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
+from .models import Item
+# 주요request 속성
+# request.method: GET,POST 요청인지 알려줌
+# request.META: META정보(요청 header포함)
+# request.GET,request.POST
 
-# Create your views here.
+# DB 추가:insert, 수정:update , 삭제:delete
+
+
+def achieve_year(request, year):
+    return HttpResponse('{}년도에 대한내용'.format(year))
+
+
+def item_list(request):
+    qs = Item.objects.all()  # 호출 예정 아직 호출 X, qurey는 꼭 필요할 때 호출됨)
+    q = request.Get.get('q', '')
+    if q:
+        qs = qs.filter(neme__icontains=q)  # 대소문자 무시
+    return render(request, 'shop/item_list.html', {
+        'item_list': qs,
+        'q': q,
+    })
