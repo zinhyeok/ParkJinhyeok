95bcf28043ccfade21425ac935486b7492a15459
zinhyeok <david9pjh@gmail.com>
2021-07-17 02:10:56 +0900

    12강

--- AskdjangoBasic/askcompany/shop/models.py
+++ AskdjangoBasic/askcompany/shop/models.py
@@ -7,5 +7,9 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
