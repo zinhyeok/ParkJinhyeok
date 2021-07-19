19c65191612e8d4310b355e3114e1de7223fb6f9
zinhyeok <david9pjh@gmail.com>
2021-07-19 20:42:52 +0900

    shop application template 경로 수정 및 view수정

--- AskdjangoBasic/askcompany/shop/urls.py
+++ AskdjangoBasic/askcompany/shop/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
+from django.urls import path, register_converter
+from .converters import FourDigitYearConverter
 from . import views
+
+register_converter(FourDigitYearConverter, 'yyyy')
 app_name = 'shop'
 
-urlpatterns = [
 
+urlpatterns = [
+    path('achieve/<yyyy:year>/', views.achieve_year, name='achieve_year'),
+    path('', views.item_list, name='item_list'),
+    path('<int:pk>/', views.item_detail, name='item_detail'),
 ]
--- AskdjangoBasic/askcompany/shop/views.py
+++ AskdjangoBasic/askcompany/shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, request
 from .models import Item
 # 주요request 속성
@@ -15,10 +15,17 @@ def achieve_year(request, year):
 
 def item_list(request):
     qs = Item.objects.all()  # 호출 예정 아직 호출 X, qurey는 꼭 필요할 때 호출됨)
-    q = request.Get.get('q', '')
+    q = request.GET.get('q', '')
     if q:
         qs = qs.filter(neme__icontains=q)  # 대소문자 무시
     return render(request, 'shop/item_list.html', {  # shop/은 namespace 정확한 template을 불러주기 위해 이용
         'item_list': qs,
         'q': q,
+    })
+
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'shop/item_detail.html', {
+        'item': item
     })
