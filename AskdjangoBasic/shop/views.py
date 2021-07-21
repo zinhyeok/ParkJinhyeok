6e1010874263f002b5e1f54e34b499af114f7a40
zinhyeok <david9pjh@gmail.com>
2021-07-21 16:54:13 +0900

    30 장고 Form을 통해, 글 생성/수정 구현하기

--- AskdjangoBasic/shop/views.py
+++ AskdjangoBasic/shop/views.py
@@ -1,8 +1,10 @@
+import re
+import logging
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse, request
 from .models import Item
-import re
-import logging
+from .forms import ItemForm
+from django.views.generic import CreateView, UpdateView
 # 주요request 속성
 # request.method: GET,POST 요청인지 알려줌
 # request.META: META정보(요청 header포함)
@@ -33,70 +35,21 @@ def item_detail(request, pk):
     })
 
 
-def item_new(request, item=None):
-    error_list = []
-    initial = {}
+item_new = CreateView.as_view(model=Item, form_class=ItemForm)
+item_edit = UpdateView.as_view(model=Item, form_class=ItemForm)
+# def item_new(request, item=None):
+#     if request.method == 'POST':
+#         form = ItemForm(Required.POST, request.FILES)
+#         if form.is_valid():
+#             item = form.save()  # MODELForm에서 제공
+#     else:
+#         form = ItemForm()
 
-    if request.method == 'POST':
-        data = request.POST
-        files = request.FILES
-
-        name = data.get('name')
-        desc = data.get('desc')
-        price = data.get('price')
-        photo = files.get('photo')
-        is_publish = data.get('is_publish') in (True, 't', 'True', '1')
-
-        # 유효성 검사
-        if len(name) < 2:
-            error_list.append('name을 2글자 이상 입력해주세요.')
-
-        if re.match(r'^[\da-zA-Z\s]+$', desc):
-            error_list.append('한글을 입력해주세요.')
-
-        if not error_list:
-            # 저장 시도
-            if item is None:
-                item = Item()
-
-            item.name = name
-            item.desc = desc
-            item.price = price
-            item.is_publish = is_publish
-
-            if photo:
-                item.photo.save(photo.name, photo, save=False)
-
-            try:
-                item.save()
-            except Exception as e:
-                error_list.append(e)
-            else:
-                return redirect(item)
-
-        initial = {
-            'name': name,
-            'desc': desc,
-            'price': price,
-            'photo': photo,
-            'is_publish': is_publish,
-        }
-    else:
-        if item is not None:
-            initial = {
-                'name': item.name,
-                'desc': item.desc,
-                'price': item.price,
-                'photo': item.photo,
-                'is_publish': item.is_publish,
-            }
-
-    return render(request, 'shop/item_form.html', {
-        'error_list': error_list,
-        'initial': initial,
-    })
+#     return render(request, 'shop/item_form.html', {
+#         'form': form
+#     })
 
 
-def item_edit(request, pk):
-    item = get_object_or_404(Item, pk=pk)
-    return item_new(request, item)
+# def item_edit(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     return item_new(request, item)
