0cca3a8fd51bae068137bf12b032cf75e8f1d03f
zinhyeok <david9pjh@gmail.com>
2021-07-22 00:05:09 +0900

    (중급)03 프로필 페이지 구현하기

--- AskdjangoBasic/askdjango/accounts/urls.py
+++ AskdjangoBasic/askdjango/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('profile/', views.profile, name="profile")
 ]
--- AskdjangoBasic/askdjango/askdjango/urls.py
+++ AskdjangoBasic/askdjango/askdjango/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-    path('', lambda req: redirect('blog:post_list')),  # url reverse
+    path('', lambda req: redirect('blog:post_list'), name='root'),  # url reverse
 ]
