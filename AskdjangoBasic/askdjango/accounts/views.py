cff8b1d0e300b424b155ae123bca08eb653a2b6a
zinhyeok <david9pjh@gmail.com>
2021-07-22 01:33:23 +0900

    (중급)08 회원가입 시에 next 인자 처리하기

--- AskdjangoBasic/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/accounts/views.py
@@ -25,21 +25,24 @@ class SignupView(CreateView):
 
 signup = SignupView.as_view()
 
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             return redirect(settings.LOGIN_URL)
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'accounts/signup.html', {
-#         'form': form,
-#     })
-signup = CreateView.as_view(model=User,
-                            form_class=UserCreationForm,
-                            template_name="accounts/signup.html",
-                            success_url=settings.LOGIN_URL)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            next_url = request.GET.get('next') or 'profile'
+            return redirect(next_url)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {
+        'form': form,
+    })
+
+# signup = CreateView.as_view(model=User,
+#                             form_class=UserCreationForm,
+#                             template_name="accounts/signup.html",
+#                             success_url=settings.LOGIN_URL)
 
 
 @login_required
