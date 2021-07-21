6cfbcc0aca845856d8869f2a10521838e9c5d695
zinhyeok <david9pjh@gmail.com>
2021-07-22 01:18:12 +0900

    (중급)06 가입과 동시에 Profile 만들기

--- AskdjangoBasic/askdjango/accounts/models.py
+++ AskdjangoBasic/askdjango/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.conf import settings
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    website_url = models.URLField(blank=True)
+
+
+def on_post_save_for_user(sender, **kwargs):
+    if kwargs['created']:
+        user = kwargs['instance']
+        Profile.objects.create(user=user)
+
+
+post_save.connect(on_post_save_for_user, sender=settings.AUTH_USER_MODEL)
