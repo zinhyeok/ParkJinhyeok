949f062c17e7a61cd3d5ee36fd4525bb8814fd42
zinhyeok <david9pjh@gmail.com>
2021-07-22 00:47:13 +0900

    (중급)05 회원가입 구현하기

--- AskdjangoBasic/askdjango/accounts/urls.py
+++ AskdjangoBasic/askdjango/accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/', views.profile, name="profile")
+    path('profile/', views.profile, name="profile"),
+    path('signup/', views.signup, name="signup")
 ]
--- AskdjangoBasic/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/accounts/views.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
 # Create your views here.
+
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             return redirect(settings.LOGIN_URL)
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'accounts/signup.html', {
+#         'form': form,
+#     })
+signup = CreateView.as_view(model=User,
+                            form_class=UserCreationForm,
+                            template_name="accounts/signup.html",
+                            success_url=settings.LOGIN_URL)
 
 
 @login_required
