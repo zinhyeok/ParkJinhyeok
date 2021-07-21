ac4d43d4307234be294bae7c1ca7f04f00306401
zinhyeok <david9pjh@gmail.com>
2021-07-22 01:25:50 +0900

    (중급)07 회원가입과 동시에 로그인하기

--- AskdjangoBasic/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/accounts/views.py
@@ -1,12 +1,29 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, resolve_url
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
-
+from django.contrib.auth import login as auth_login
 # Create your views here.
+from django.contrib.auth import login as auth_login
 
+
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect('profile')
+
+
+signup = SignupView.as_view()
 
 # def signup(request):
 #     if request.method == 'POST':
